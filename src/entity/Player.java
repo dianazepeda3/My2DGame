8c3fe457135985728d53bd96d8f4c8833c55f7a7
Diana Zepeda Tatengo <diana.zepeda6085@alumnos.udg.mx>
2025-12-18 13:00:22 -0600

    Estado del Juego

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -20,7 +20,7 @@ public class Player extends Entity{
 	
 	public final int screenX;
 	public final int screenY;
-	public int hasKey = 0;
+	//public int hasKey = 0;
 	int standCounter = 0;
 	
 	public Player(GamePanel gp, KeyHandler keyH) {
@@ -172,38 +172,7 @@ public class Player extends Entity{
 	
 	public void pickUpObject(int index) {
 		if(index != 999) {
-			String objectName = gp.obj[index].name;
 			
-			switch(objectName) {
-			case "Key":
-				gp.playSE(1);
-				hasKey++;
-				gp.obj[index] = null;
-				gp.ui.showMessage("You got a key!");
-				break;
-			case "Door":
-				if(hasKey > 0) {
-					gp.playSE(3);
-					gp.obj[index] = null;
-					hasKey--;
-					gp.ui.showMessage("You opened the door!");
-				}else {
-					gp.ui.showMessage("You need a key!");
-				}
-				break;
-			case "Boots":
-				gp.playSE(2);
-				speed += 2;
-				gp.obj[index] = null;
-				gp.ui.showMessage("Speed up!");
-				break;
-			case "Chest":				
-				gp.ui.gameFinished = true;
-				gp.stopMusic();
-				gp.playSE(4);
-				break;
-			}
-			//System.out.println("Key: "+hasKey);
 		}
 	}
 }

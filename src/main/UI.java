42b6e57dd9412f1cdf198ffeca8c4df54018897e
Diana Zepeda Tatengo <diana.zepeda6085@alumnos.udg.mx>
2025-12-19 10:47:56 -0600

    Fuentes

--- src/main/UI.java
+++ src/main/UI.java
@@ -3,12 +3,16 @@ package main;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class UI {
 	GamePanel gp;
 	Graphics2D g2;
-	Font arial_40, arial_80B;
+	Font maruMonica, purisaB;
 	public boolean messageOn = false;
 	public String message = "";
 	int messageCounter = 0;
@@ -17,15 +21,27 @@ public class UI {
 	
 	public UI(GamePanel gp) {
 		this.gp = gp;
-		arial_40 = new Font("Arial", Font.PLAIN, 40);
-		arial_80B = new Font("Arial", Font.BOLD, 80);	
+		
+		try {
+			InputStream is = getClass().getResourceAsStream("/fonts/x12y16pxMaruMonica.ttf");
+			maruMonica = Font.createFont(Font.TRUETYPE_FONT, is);
+			
+			is = getClass().getResourceAsStream("/fonts/purisa.ttf");
+			purisaB = Font.createFont(Font.TRUETYPE_FONT, is);
+		} catch (FontFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void draw(Graphics2D g2) {
 		
 		this.g2 = g2;
 		
-		g2.setFont(arial_40);
+		g2.setFont(maruMonica);
+		//g2.setFont(purisaB);
+		//g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		g2.setColor(Color.white);
 		
 		// PLAY STATE 
@@ -67,7 +83,7 @@ public class UI {
 		int height = gp.tileSize*4;
 		drawSubWindow(x, y, width, height);
 		
-		g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 28F));
+		g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 32F));
 		x += gp.tileSize;
 		y += gp.tileSize;
 		

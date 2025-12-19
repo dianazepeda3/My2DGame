198946f2cfbc5de55dd786dd9f8d86c6a644b692
Diana Zepeda Tatengo <diana.zepeda6085@alumnos.udg.mx>
2025-12-18 18:22:49 -0600

    Dialogos

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -7,7 +7,7 @@ public class KeyHandler implements KeyListener{
 	
 	GamePanel gp;
 	
-	public boolean upPressed, downPressed, leftPressed, rightPressed;
+	public boolean upPressed, downPressed, leftPressed, rightPressed, enterPressed;
 	// DEBBUG
 	boolean checkDrawTime = false;
 	
@@ -24,32 +24,46 @@ public class KeyHandler implements KeyListener{
 	public void keyPressed(KeyEvent e) {
 		int code = e.getKeyCode();
 		
-		if(code == KeyEvent.VK_W) {
-			upPressed = true;
-		}
-		if(code == KeyEvent.VK_S) {
-			downPressed = true;
-		}
-		if(code == KeyEvent.VK_A) {
-			leftPressed = true;
-		}
-		if(code == KeyEvent.VK_D) {
-			rightPressed = true;
-		}
-		if(code == KeyEvent.VK_P) {
-			if(gp.gameState == gp.playState) {
-				gp.gameState = gp.pauseState;
-			}else if(gp.gameState == gp.pauseState) {
-				gp.gameState = gp.playState;
+		// PLAY STATE
+		if(gp.gameState == gp.playState) {
+			if(code == KeyEvent.VK_W) {
+				upPressed = true;
+			}
+			if(code == KeyEvent.VK_S) {
+				downPressed = true;
+			}
+			if(code == KeyEvent.VK_A) {
+				leftPressed = true;
+			}
+			if(code == KeyEvent.VK_D) {
+				rightPressed = true;
+			}
+			if(code == KeyEvent.VK_P) {
+				gp.gameState = gp.pauseState;				
+			}
+			if(code == KeyEvent.VK_ENTER) {
+				enterPressed = true;				
+			}
+			
+			// DEBUG
+			if(code == KeyEvent.VK_T) {
+				if(checkDrawTime == false) {
+					checkDrawTime = true;
+				}else if(checkDrawTime == true) {
+					checkDrawTime = false;
+				}
 			}
 		}
-		
-		// DEBUG
-		if(code == KeyEvent.VK_T) {
-			if(checkDrawTime == false) {
-				checkDrawTime = true;
-			}else if(checkDrawTime == true) {
-				checkDrawTime = false;
+		// PAUSE STATE
+		else if(gp.gameState == gp.pauseState) {
+			if(code == KeyEvent.VK_P) {
+				gp.gameState = gp.playState;
+			}
+		}		
+		// DIALOGUE STATE
+		else if(gp.gameState == gp.dialogueState) {
+			if(code == KeyEvent.VK_ENTER) {
+				gp.gameState = gp.playState;
 			}
 		}
 	}
--- src/main/UI.java
+++ src/main/UI.java
@@ -1,32 +1,24 @@
 package main;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.text.DecimalFormat;
-
-import object.OBJ_Key;
 
 public class UI {
 	GamePanel gp;
 	Graphics2D g2;
 	Font arial_40, arial_80B;
-	BufferedImage keyImage;
 	public boolean messageOn = false;
 	public String message = "";
 	int messageCounter = 0;
 	public boolean gameFinished = false;
-	
-	double playTime;
-	DecimalFormat dFormat = new DecimalFormat("#0.00");
+	public String currentDialogue = "";
 	
 	public UI(GamePanel gp) {
 		this.gp = gp;
 		arial_40 = new Font("Arial", Font.PLAIN, 40);
-		arial_80B = new Font("Arial", Font.BOLD, 80);
-		//OBJ_Key key = new OBJ_Key(gp);
-		//keyImage = key.image;
+		arial_80B = new Font("Arial", Font.BOLD, 80);	
 	}
 	
 	public void draw(Graphics2D g2) {
@@ -36,11 +28,19 @@ public class UI {
 		g2.setFont(arial_40);
 		g2.setColor(Color.white);
 		
+		// PLAY STATE 
 		if(gp.gameState == gp.playState) {
 			// Do playState stuff later
 		}
+		
+		// PAUSE STATE 
 		if(gp.gameState == gp.pauseState) {
 			drawPauseScreen();
+		}
+		
+		// DIALOGUE STATE
+		if(gp.gameState == gp.dialogueState) {
+			drawDialogueScreen();
 		}
 		
 	}
@@ -57,6 +57,36 @@ public class UI {
 	public void showMessage(String text) {
 		message = text;
 		messageOn = true;
+	}
+	
+	public void drawDialogueScreen() {
+		// WINDOW
+		int x = gp.tileSize*2;
+		int y = gp.tileSize/2;
+		int width = gp.screenWidth - (gp.tileSize*4);;
+		int height = gp.tileSize*4;
+		drawSubWindow(x, y, width, height);
+		
+		g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 28F));
+		x += gp.tileSize;
+		y += gp.tileSize;
+		
+		for(String line : currentDialogue.split("\n")) {
+			g2.drawString(line, x, y);
+			y += 40;
+		}		
+	}	
+	
+	public void drawSubWindow(int x, int y, int width, int height) {
+		Color c = new Color(0, 0, 0, 210);
+		g2.setColor(c);
+		g2.fillRoundRect(x, y, width, height, 35, 35);		
+		
+		c = new Color(255, 255, 255);
+		g2.setColor(c);
+		g2.setStroke(new BasicStroke(5));
+		g2.drawRoundRect(x+5, y+5, width-10, height-10, 25, 25);
+		
 	}
 	
 	public int getXforCenteredText(String text) {
